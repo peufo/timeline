e26237e5ff18ee211a2b6f5225bcf338594d9b09
jonas <jonas.voisard@gmail.com>
2022-12-13 13:49:03 +0100

    Fix readable Timeline

--- src/lib/TimeLine.svelte
+++ src/lib/TimeLine.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import type { TimeLineEvent } from '$lib/types'
   import '$lib/timeline.scss'
 
@@ -17,10 +18,22 @@
     hour: 'numeric',
     minute: 'numeric',
   })
+
+  let styleElement: HTMLStyleElement
+  onMount(() => {
+    styleElement = document.createElement('style')
+    document.head.appendChild(styleElement)
+    return () => {
+      document.head.removeChild(styleElement)
+    }
+  })
+  $: if (styleElement) {
+    styleElement.innerHTML = style
+  }
 </script>
 
 <div bind:this={timelineElement} class="timeline {klass}" {style} class:hasNext>
-  {#each events as { title, detail, time, date }, index}
+  {#each events as { title, detail, time, date, subEvents }, index}
     <div class="time">
       {!!date ? intl.format(date) : time}
     </div>
@@ -33,8 +46,11 @@
     </div>
 
     <div class="content">
-      <b>{title}</b>
-      <div>{detail}</div>
+      <b class="title">{title}</b>
+      <p class="detail">{detail}</p>
+      {#if subEvents && subEvents.length}
+        <svelte:self bind:events={subEvents} {hasNext} {style} class={klass} />
+      {/if}
     </div>
   {/each}
 </div>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,5 +3,5 @@ export type TimeLineEvent = {
   time?: string
   date?: Date
   detail?: string
-  subEvents?: TimelineEvent[]
+  subEvents?: TimeLineEvent[]
 }
